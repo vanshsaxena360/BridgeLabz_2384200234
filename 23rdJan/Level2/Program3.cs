9a442b3e235f389d1ad5ae1209b2f532f2aa3947
vanshsaxena360 <100285405+vanshsaxena360@users.noreply.github.com>
2025-01-25 09:15:02 +0530

    Level 2 Program 3 done

---
+++ 23rdJan/Level2/Program3.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program{
+	static void Main(string[] args){
+		//Prompt the user to enter a number
+		Console.Write("Enter the Number: ");
+		int num = int.Parse(Console.ReadLine());
+		
+		// we are replacing Fizz instead of 3 and its multiple, Buzz instead of 5 and its multiple, FizzBuzz instead of 3X5 and its multiple using while loop 
+		int i=1;
+		while(i<=num){
+			if(i%3==0 && i%5==0){
+				Console.WriteLine("FizzBuzz");
+			}
+			else if (i%3==0){
+				Console.WriteLine("Fizz");
+			}
+			else if(i%5==0){
+				Console.WriteLine("Buzz");
+			}
+			else{
+				Console.WriteLine(i);
+			}
+			i++;
+		}
+	}
+}
