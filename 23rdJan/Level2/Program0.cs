af5debf0978ced87d93c19f97d85216153e47df8
vanshsaxena360 <100285405+vanshsaxena360@users.noreply.github.com>
2025-01-25 09:12:38 +0530

    Level 2 Program 0 done

---
+++ 23rdJan/Level2/Program0.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program{
+	static void Main(string[] args){
+		// Prompt the user to enter the year
+		Console.Write("Enter the year: ");
+		int year = int.Parse(Console.ReadLine());
+		// if statement to check the year after 1582
+		if (year<=1582){
+			Console.WriteLine("Enter the year after 1582");
+		}
+		else{		
+			// if and nested if statements to check whether is the leap year or not
+			if (year%4==0){
+				if (year%100==0){
+					if (year%400==0){
+						Console.WriteLine("This is the leap year");
+					}
+					else{
+						Console.WriteLine("Not a leap year");
+					}
+				}
+				else{
+					Console.WriteLine("This is the leap year");
+				}
+			}
+			else{
+				Console.WriteLine("Not a leap year");
+			}
+		}
+	}
+}
