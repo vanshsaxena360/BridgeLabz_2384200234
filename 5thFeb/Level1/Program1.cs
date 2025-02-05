4750de8cd6e0a2472b64b30932cb11c95d37b284
vanshsaxena360 <100285405+vanshsaxena360@users.noreply.github.com>
2025-02-05 13:10:32 +0530

    level 1 program 1 done

---
+++ 5thFeb/Level1/Program1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Book
+{
+    public string Title { get; set; }
+
+    public Book(string title)
+    {
+        Title = title;
+    }
+}
+
+class Library
+{
+    public List<Book> Books = new List<Book>();
+
+    public void AddBook(Book book)
+    {
+        Books.Add(book);
+    }
+
+    public void ShowBooks()
+    {
+        foreach (var book in Books)
+        {
+            Console.WriteLine(book.Title);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book book1 = new Book("The Great Gatsby");
+        Book book2 = new Book("1984");
+
+        Library library = new Library();
+        library.AddBook(book1);
+        library.AddBook(book2);
+
+        library.ShowBooks();
+    }
+}
