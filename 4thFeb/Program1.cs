be2008515c552cbdd496991a7d81fa8647e21b25
vanshsaxena360 <100285405+vanshsaxena360@users.noreply.github.com>
2025-02-05 11:05:14 +0530

    Level 1 Program 1 done

---
+++ 4thFeb/Program1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class BankAccount
+{
+    private static string bankname = "HDFC";
+    private static int totalAccounts = 0; // countering for total accounts
+
+    // readonly variable ==> cannot change
+    public readonly int AccountNumber;
+
+    // instance variables
+    private string AccountHolderName;
+    private decimal Balance;
+
+    public BankAccount(int accountNumber, string accountHolderName, decimal initialBalance)
+    {
+        this.AccountNumber = accountNumber;
+        this.AccountHolderName = accountHolderName;
+        this.Balance = initialBalance;
+        // increasing total accounts
+        totalAccounts++;
+    }
+
+    // method to display account details
+    public void DisplayAccount() {
+        if (this is BankAccount) {
+            Console.WriteLine("Bank: "+bankname+", Account No.: "+AccountNumber+", Holder: "+AccountHolderName+", Balance: "+Balance);
+        }
+    }
+
+    public static void ShowTotalAccounts() {
+        Console.WriteLine("Total accounts: " + totalAccounts);
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        BankAccount acc1 = new BankAccount(1001, "Aman",2000);
+        BankAccount acc2 = new BankAccount(1002, "Rahul",3000);
+
+
+        // display the accounts
+        acc1.DisplayAccount();
+        acc2.DisplayAccount();
+
+        // showing total accounts
+        BankAccount.ShowTotalAccounts();
+    }
+}
