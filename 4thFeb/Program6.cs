ba01241a4f0a2c415b2c744605bd9de9c4dfd3a6
vanshsaxena360 <100285405+vanshsaxena360@users.noreply.github.com>
2025-02-05 11:08:27 +0530

    Level 1 Program 6 done

---
+++ 4thFeb/Program6.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Vehicle
+{
+    // Static variable shared across all vehicles
+    public static double RegistrationFee = 5000;
+    // Readonly variable for unique registration number
+    public readonly string RegistrationNumber;
+
+    // Instance variables
+    public string OwnerName;
+    public string VehicleType;
+
+    // Constructor
+    public Vehicle(string regNumber, string owner, string type)
+    {
+        this.RegistrationNumber = regNumber;
+        this.OwnerName = owner;
+        this.VehicleType = type;
+    }
+
+    // Method to display vehicle details
+    public void DisplayVehicleDetails()
+    {
+        Console.WriteLine("Owner: " + OwnerName +", Vehicle Type: "+VehicleType+", Registration Number: "+RegistrationNumber+", Fee: "+RegistrationFee);
+    }
+
+    // Static method to update registration fee
+    public static void UpdateRegistrationFee(double newFee)
+    {
+        RegistrationFee = newFee;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating vehicle objects
+        Vehicle v1 = new Vehicle("V12345", "Aman", "Car");
+        Vehicle v2 = new Vehicle("V67890", "Rahul", "Bike");
+
+        // Checking if vehicle1 is an instance of Vehicle class using 'is' operator
+        if (v1 is Vehicle)
+        {
+            v1.DisplayVehicleDetails();
+        }
+
+        if (v2 is Vehicle)
+        {
+            v2.DisplayVehicleDetails();
+        }
+
+        // Updating registration fee
+        Vehicle.UpdateRegistrationFee(5500.0);
+        Console.WriteLine("\nUpdated Registration Fee Applied!");
+        // Displaying details after updating fee
+        v1.DisplayVehicleDetails();
+        v2.DisplayVehicleDetails();
+    }
+}
